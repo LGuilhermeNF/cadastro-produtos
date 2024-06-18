57d89b41ae4dc65fbdadc64777be9ead03e95414
Guilherme Fernandes <guilherme.doze@hotmail.com>
2024-06-18 16:43:03 -0300

    atualização do projeto

--- src/rotas.jsx
+++ src/rotas.jsx
@@ -2,13 +2,13 @@ import React from "react";
 import { HashRouter, Route, Routes } from "react-router-dom";
 
 import Home from "./views/home";
-import { CadstroProduto } from "./views/produtos/cadastro";
+import { CadastroProduto } from "./views/produtos/cadastro";
 
 export default () => {
   return (
     <HashRouter>
       <Routes>
-        <Route exact path="/cadastro-produtos" element={<CadstroProduto />} />
+        <Route exact path="/cadastro-produtos" element={<CadastroProduto />} />
         <Route exact path="/" element={<Home />} />
       </Routes>
     </HashRouter>
--- src/views/produtos/cadastro.jsx
+++ src/views/produtos/cadastro.jsx
@@ -1,11 +1,132 @@
 import React from "react";
+import { ProdutoService } from "../../app/produtoService";
 
-export class CadstroProduto extends React.Component {
+const estadoInicial = {
+    nome: '',
+    sku: '',
+    descricao: '',
+    preco: 0,
+    fornecedor: '',
+    sucesso: false
+}
+
+export class CadastroProduto extends React.Component {
+  
+  state = estadoInicial;
+
+  constructor(){
+    super();
+    this.service = new ProdutoService;
+  }
+  
+  onSubmit = (event) => {
+    const produto = {
+      nome: this.state.nome,
+      sku: this.state.sku,
+      descricao: this.state.descricao,
+      preco: this.state.preco,
+      fornecedor: this.state.fornecedor
+    }
+    this.service.salvar(produto);
+    this.limpaCampos();
+    this.setState({sucesso: true})
+    
+  }
+
+  limpaCampos = () => {
+    this.setState(estadoInicial);
+  }
+
   render() {
     return (
+      
       <div className="card">
         <div className="card-header">Cadastro de Produto</div>
-        <div className="card-body"></div>
+        <div className="card-body">
+          
+          {this.state.sucesso &&
+            ( <div class="alert alert-dismissible alert-success">
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                <strong>Sucesso!</strong> Produto cadastrado.
+              </div>
+            )
+          }
+
+          <div className="row mb-3">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Nome: *</label>
+                <input 
+                  type="text" 
+                  value={this.state.nome} 
+                  onChange={(event) => {this.setState({nome: event.target.value })}} 
+                  className="form-control" 
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>SKU: *</label>
+                <input 
+                  type="text" 
+                  value={this.state.sku} 
+                  onChange={(event) => {this.setState({sku: event.target.value })}} 
+                  className="form-control" 
+                />
+              </div>
+            </div>
+          </div>
+
+          <div classNbame="row mb-6">
+            
+            <div className="col-md-12">
+              <div className="form-group">
+                <label>Descrição:</label>
+                <textarea 
+                  value={this.state.descricao} 
+                  onChange={(event) => {this.setState({descricao: event.target.value })}} 
+                  className="form-control">
+                </textarea>
+              </div>
+            </div>
+          
+          </div>
+
+          <div className="row mt-3">
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Preço: *</label>
+                <input 
+                  type="text" value={this.state.preco} 
+                  onChange={(event) => {this.setState({preco: event.target.value })}} 
+                  className="form-control" 
+                />
+              </div>
+            </div>
+
+            <div className="col-md-6">
+              <div className="form-group">
+                <label>Fornecedor: *</label>
+                <input 
+                  type="text" 
+                  value={this.state.fornecedor} 
+                  onChange={(event) => {this.setState({fornecedor: event.target.value })}} 
+                  className="form-control" 
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="row mt-3">
+            <div className="col-md-1">
+              <button className="btn btn-success" onClick={this.onSubmit}>Salvar</button>
+            </div>
+            <div className="col-md-1">
+              <button className="btn btn-light" onClick={this.limpaCampos}>Limpar</button>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
